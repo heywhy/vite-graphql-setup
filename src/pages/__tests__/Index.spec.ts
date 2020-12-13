eef15c8e791993e80b850bb1ad68c44f62f8e0e6
atanda rasheed <atandarash@gmail.com>
2020-12-14 00:44:09 +0100

    chore: update @testing-library/vue

--- src/pages/__tests__/Index.spec.ts
+++ src/pages/__tests__/Index.spec.ts
@@ -6,13 +6,13 @@ import { fireEvent, render } from '@testing-library/vue'
 const routes = [{ path: '/', component: Index }]
 
 test('renders index page', async () => {
-  const { findByText, findByAltText } = render(App, { routes } as any)
+  const { findByText, findByAltText } = render(App, { routes })
   expect(await findByAltText('Vue logo')).toBeInTheDocument()
   expect(await findByText('count is: 0')).toBeInTheDocument()
 })
 
 test('increment count on button clicked', async () => {
-  const { findByText, findByTestId } = render(App, { routes } as any)
+  const { findByText, findByTestId } = render(App, { routes })
   const btn = await findByTestId('count-btn')
 
   await fireEvent.click(btn)
