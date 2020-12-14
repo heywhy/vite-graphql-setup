c168c0d5ab3fec2aec9534732cfb348368850d61
atanda rasheed <atandarash@gmail.com>
2020-12-14 01:05:14 +0100

    chore: update index page

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,6 +1,6 @@
 <template>
   <img class="inline-block" alt="Vue logo" src="../assets/logo.png" />
-  <HelloWorld msg="Hello Vue 3.0 + Vite" />
+  <HelloWorld msg="Hello Vue 3.0 + Vite + Vue Apollo + Tailwind" />
 </template>
 
 <script lang="ts">
@@ -10,7 +10,7 @@ import HelloWorld from '../components/HelloWorld.vue'
 export default defineComponent({
   name: 'App',
   components: {
-    HelloWorld
-  }
+    HelloWorld,
+  },
 })
 </script>
