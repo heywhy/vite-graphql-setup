077caaffebf4aaaf960a38efab7eff915907a201
atanda rasheed <atandarash@gmail.com>
2021-01-20 02:32:50 +0100

    chore: update to vite v2

---
+++ vite.config.ts
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+
+export default defineConfig({
+  plugins: [vue()],
+  optimizeDeps: {
+    include: ['@apollo/client/core'],
+    exclude: ['@apollo/client'],
+  },
+})
